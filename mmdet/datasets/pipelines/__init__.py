ae3c8c24ef150ba42c8a40005642ffdf7925e4c2
Kari Meling Johannessen <karimelingjohannessen@gmail.com>
2020-01-06 15:44:06 +0100

    Update __init__.py

--- mmdet/datasets/pipelines/__init__.py
+++ mmdet/datasets/pipelines/__init__.py
@@ -6,22 +6,12 @@ from .loading import LoadAnnotations, LoadImageFromFile, LoadProposals
 from .test_aug import MultiScaleFlipAug
 from .transforms import (Albu, Expand, MinIoURandomCrop, Normalize, Pad,
                          PhotoMetricDistortion, RandomCrop, RandomFlip, Resize,
-<<<<<<< HEAD
-                         SegResizeFlipPadRescale)
-from .custom_transforms import (Flip)
-=======
                          SegRescale)
->>>>>>> e907139cb8f4c0195abcf31101925819f369ecc1
 
 __all__ = [
     'Compose', 'to_tensor', 'ToTensor', 'ImageToTensor', 'ToDataContainer',
     'Transpose', 'Collect', 'LoadAnnotations', 'LoadImageFromFile',
     'LoadProposals', 'MultiScaleFlipAug', 'Resize', 'RandomFlip', 'Pad',
-<<<<<<< HEAD
-    'RandomCrop', 'Normalize', 'SegResizeFlipPadRescale', 'MinIoURandomCrop',
-    'Expand', 'PhotoMetricDistortion', 'Albu','Flip'
-=======
     'RandomCrop', 'Normalize', 'SegRescale', 'MinIoURandomCrop', 'Expand',
     'PhotoMetricDistortion', 'Albu', 'InstaBoost'
->>>>>>> e907139cb8f4c0195abcf31101925819f369ecc1
 ]
