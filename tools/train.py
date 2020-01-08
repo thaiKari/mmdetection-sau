83629ee586b95f1bb906296224c111b33e06160d
thaiKari <karimelingjohannessen@gmail.com>
2020-01-08 14:35:15 +0100

    run training on new computer with new train

--- tools/train.py
+++ tools/train.py
@@ -55,8 +55,6 @@ def parse_args():
 
 def main():
     args = parse_args()
-    print('================ARGS==================')
-    print(args)
 
     cfg = Config.fromfile(args.config)
     # set cudnn_benchmark
